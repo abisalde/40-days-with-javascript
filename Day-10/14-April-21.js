ae37eac41bee3898d4aaf0cc687062505a17abc9
Isaiah <isaiah.abiodun@outlook.com>
2021-04-14 10:44:02 -0400

    🖌️Style CV🖌️

---
+++ Day-10/14-April-21.js
@@ -0,0 +1,8 @@
+/***
+ *
+ * https://www.codewars.com/kata/5544c7a5cb454edb3c000047/javascript
+ */
+
+const bouncingBall = (h, bounce, window) => {};
+
+console.log(bouncingBall(3.0, 0.66, 1.5), 3);
